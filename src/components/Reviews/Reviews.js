927e9d9c42b75c4fd6810ebbf9f56e2004ac4055
Valentun2 <pocomaxawrkt@gmail.com>
2023-10-26 21:26:15 +0300

    update

--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -5,6 +5,7 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Li } from './Reviews.styled';
 
+
 const Reviews = () => {
   const [reviews, setReviews] = useState([]);
   const param = useParams();
